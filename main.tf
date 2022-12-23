e379a37d9fdc3fc4d81acb3ccc353d53cbfd8a99
Levente Simon <ls@leventesimon.com>
2022-12-23 11:36:16 +0100

    do not load k8s config file for kubectl

--- main.tf
+++ main.tf
@@ -27,4 +27,5 @@ provider "kubectl" {
   client_certificate     = var.k8s_client_certificate
   client_key             = var.k8s_client_key
   cluster_ca_certificate = var.k8s_cluster_ca_certificate
+  load_config_file       = false
 }
